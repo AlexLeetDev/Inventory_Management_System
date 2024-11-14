035ff3cb526951011fed4fdfd49b44abc7e93146
Alex Leet <alex@alexleet.com>
2024-11-14 18:11:41 -0500

    Updated admin.py, settings, custom CSS, and base_site.html; removed base.html

--- inventory/admin.py
+++ inventory/admin.py
@@ -7,29 +7,29 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('icon', 'name', 'price', 'stock')
 
     def icon(self, obj):
-        return format_html('<i class="fas fa-box"></i>')
-    icon.short_description = 'Icon'
+        return format_html('<i class="fa-sharp-duotone fa-box"></i>')
+    icon.short_description = ''
 
 @admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ('icon', 'name')
 
     def icon(self, obj):
-        return format_html('<i class="fas fa-truck"></i>')
-    icon.short_description = 'Icon'
+        return format_html('<i class="fa-duotone fa-truck"></i>')
+    icon.short_description = ''
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('icon', 'first_name', 'last_name')
 
     def icon(self, obj):
-        return format_html('<i class="fas fa-user"></i>')
-    icon.short_description = 'Icon'
+        return format_html('<i class="fa-duotone fa-user"></i>')
+    icon.short_description = ''
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ('icon', 'product', 'customer', 'date_ordered')
 
     def icon(self, obj):
-        return format_html('<i class="fas fa-shopping-cart"></i>')
-    icon.short_description = 'Icon'
+        return format_html('<i class="fa-sharp-duotone fa-cart-shopping"></i>')
+    icon.short_description = ''
